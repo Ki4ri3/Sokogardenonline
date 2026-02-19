9a8362adb1db642234cea05e47e440a01a2e472b
Ki4ri3 <lewiskiarie9@gmail.com>
2026-02-19 17:20:34 +0300

    signin

--- app.py
+++ app.py
@@ -42,14 +42,43 @@ def signup():
         return jsonify({"message" : "User registered successfully."})
 
 
+# Below is the login/sign in route.
+@app.route("/api/signin", methods = ["POST"])
+def signin():
+    if request.method == "POST":
+        # Extract the two details entered on the form
+        email = request.form["email"]
+        password = request.form["password"]
+
+        # Print out the details on the form
+        # print(email,password)
+        # Create/establish a connection.
+        connection = pymysql.connect(host="localhost",user="root",password="",database="sokogardenonline")
+
+        # Create a cursor
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        # Structure the sql query that will whether the email and password entered are correct.
+        sql = "SELECT * FROM users WHERE  email = %s AND password = %s"
+
+        # Put the data received from the form into a tuple.
+        data = (email,password)
+
+        # By use of the cursor execute the sql
+        cursor.execute(sql,data)
 
 
+        # Check whether there are rows returned and store the on a variable
+        count = cursor.rowcount
+        #  print(count)
 
-
-
-        return jsonify({"message" : "The sign up route accessed. "})
-
-
+        # If there are records returned,the password and the email are correct,otherwise,they are wrong
+        if count == 0:
+            return jsonify({"message" : "Login Failed"})
+        else:
+            # There must be  a user so we create a variable that will hold the details of the user fetched from the database.
+            user=cursor.fetchone()
+            # Return the details to the front end as well as a message
+            return jsonify({"message" : "User Logged In Successfully", "user":user})
 
 
 
