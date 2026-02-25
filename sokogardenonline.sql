6651a60ca6cbe84203cf76f8f0a1df94abc8ffc3
Ki4ri3 <lewiskiarie9@gmail.com>
2026-02-25 12:10:20 +0300

    updated

---
+++ sokogardenonline.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 25, 2026 at 10:04 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sokogardenonline`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_details`
+--
+
+CREATE TABLE `product_details` (
+  `product_id` int(50) NOT NULL,
+  `product_name` varchar(200) NOT NULL,
+  `product_description` varchar(2000) NOT NULL,
+  `product_cost` int(50) NOT NULL,
+  `product_photo` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_details`
+--
+
+INSERT INTO `product_details` (`product_id`, `product_name`, `product_description`, `product_cost`, `product_photo`) VALUES
+(1, 'Mini Phone', 'This is a small phone', 12000, '<FileStorage: \'smartphone.jpg\' (\'image/jpeg\')>'),
+(2, 'Mini Phone', 'This is a small phone', 12000, 'smartphone.jpg'),
+(3, 'Xiaomi TV A Pro', ' Larger, more cinematic sibling in Xiaomi’s latest QLED lineup', 52000, 'tv.jpg'),
+(4, 'Camera', '64MP 16X Optical Zoom Digital Camera for Photography with 32GB TF Card', 17645, 'camera.jpg'),
+(5, 'HP Pro Book Laptop', 'HP ProBook 640 G5 | i5 8th Gen | 16GB 512GB SSD | 14\'\' | Windows 11 Pro\n', 27000, 'laptop.jpg'),
+(6, 'Power Bank', ' 20000mAh Portable Power Bank with 3 In 1 Built-In Cable Android, iPhone and Type C', 999, 'powerbank.jpg'),
+(7, 'Washing Machine', '12kg Front Load Washing Machine – Efficient Cleaning', 63000, 'washingmachine.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `phone`, `password`) VALUES
+(1, 'Kiarie', 'kiarie1@gmail.com', '254786432987', '1234'),
+(2, 'joseph', 'joseph@gmail.com', '254754321234', '4321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product_details`
+--
+ALTER TABLE `product_details`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`,`email`,`phone`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product_details`
+--
+ALTER TABLE `product_details`
+  MODIFY `product_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
